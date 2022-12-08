a487f65200175143a8ebe20bfb3aef8b859e0688
Theophilus Tay <jaybuck403@gmail.com>
2022-12-08 10:41:32 -0800

    Hello World - C

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,7 +6,7 @@
 
 int main(void)
 {
-	char msg[] = "with proper grammer, but the outcome is a piece of art,";
+	char msg[] = "with proper grammar, but the outcome is a piece of art,";
 
 	printf("%s\n", msg);
 	return (0);
