baa869b99db2b706ff3879719c2093d409304e71
Theophilus Tay <jaybuck403@gmail.com>
2022-12-08 10:37:40 -0800

    Hello World - C

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a string
+ */
+
+int main(void)
+{
+	char msg[] = "with proper grammer, but the outcome is a piece of art";
+
+	printf("%s\n", msg);
+	return (0);
+}
