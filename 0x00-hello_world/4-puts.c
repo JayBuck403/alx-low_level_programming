7051f0b3ea43393f6787e8a0a0c97bcbed34245c
Theophilus Tay <jaybuck403@gmail.com>
2022-12-08 10:21:45 -0800

    Hello World - C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - Prints "Programming is like building a multilingual puzzle
+ */ 
+
 int main(void)
 {
 	char msg[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(msg);
 
-	return 0;
+	return (0);
 }
