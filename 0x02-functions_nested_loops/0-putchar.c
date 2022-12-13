77c9cfed6076e81f2f9b072aa387b908193c8503
Theophilus Tay <jaybuck403@gmail.com>
2022-12-13 04:46:17 -0800

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,23 +7,21 @@
  * _putchar - writes the characters _putchar to stdout
  * @c: The character to print
  *
- * Return: On success 1
- * On error, -1 is returned, and errno is set appropriately
  */
 
-int _putchar(char c);
+int _putchar(int c);
 
 int main(void)
 {
-        _putchar('_');
-        _putchar('p');
-        _putchar('u');
-        _putchar('t');
-        _putchar('c');
-        _putchar('h');
-        _putchar('a');
-        _putchar('r');
-        _putchar('\n');
-        return (0);
+	 _putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
 
