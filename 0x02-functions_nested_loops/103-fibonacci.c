19598128942c0e0af821aa10eb1fc0061dad8c4e
Theophilus Tay <jaybuck403@gmail.com>
2022-12-14 03:55:23 -0800

    fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+  * main - prints the sum of even fibonacci 
+  * Return: 0
+  */
+int main(void)
+{
+	unsigned long n;
+	unsigned long i;
+	unsigned long j;
+	unsigned long k;
+	unsigned long sum;
+
+	i = 0;
+	sum = 0;
+	j = 1;
+	for (n = 0; n < 50; n++)
+	{
+		k = i + j;
+		i = j;
+		j = k;
+		if (k % 2 == 0 && k < 4000000)
+		{
+			sum += k;
+		}
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
