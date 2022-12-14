d9cb8f7cce4d640af05ca26c5b16f3f0a5600d73
Theophilus Tay <jaybuck403@gmail.com>
2022-12-14 02:58:20 -0800

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,7 +3,7 @@
 
 /**
  * print_to_98 - prints numbers to 98
- *
+ * @n: input to count from
  * Return: void
  */
 
@@ -11,7 +11,7 @@ void print_to_98(int n)
 {
 	if (n < 98)
 	{
-		for (n =n; n < 98; n++)
+		for (n = n; n < 98; n++)
 		{
 			printf("%d, ", n);
 		}
