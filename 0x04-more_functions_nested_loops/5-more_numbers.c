fc1040aca4a92679a9d542a8da529b529e21c6dc
Theophilus Tay <jaybuck403@gmail.com>
2022-12-15 04:56:07 -0800

    Straight line

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+
+void more_numbers(void)
+{
+	int j;
+
+	while (j < 10)
+	{
+		int i;
+
+		for (i = 48; i <= 57; i++)
+		{
+			_putchar(i);
+		}
+		_putchar9('\n');
+		j++
+	}
+}
