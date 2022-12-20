149e29a5aba2786096e2f7953fbb5431219a741e
Theophilus Tay <jaybuck403@gmail.com>
2022-12-20 05:17:33 -0800

    Puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: string input
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int i = _strlen(str) % 2;
+	int j = _strlen(str) / 2;
+	int k = (_strlen(str) - 1) / 2;
+
+	while (i == 0 && str[j] != '\0')
+	{
+		putchar(str[j]);
+		j++;
+	}
+
+	while (i != 0 && str[k] !='\0')
+	{
+		putchar(str[k]);
+		k++;
+	}
+	putchar('\n');
+}
+
+/**
+ * _strlen - computes the length of a string
+ * @s: string input
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
