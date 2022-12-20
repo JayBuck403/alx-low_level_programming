c6e6260f19563e570f215bbff21a28ae7ef874c7
Theophilus Tay <jaybuck403@gmail.com>
2022-12-20 05:05:30 -0800

    Reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: string to print in reverse
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = _strlen(s) - 1; i >= 0; i--)
+		putchar(s[i]);
+	putchar('\n');
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string input
+ *
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
