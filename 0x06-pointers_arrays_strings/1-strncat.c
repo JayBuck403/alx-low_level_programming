3088f97867eaf4067d16de7e9e6d6d7e88a29448
Theophilus Tay <jaybuck403@gmail.com>
2022-12-21 06:46:44 -0800

    Strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: destination 
+ * @src: source
+ *
+ * Return: concatenated string to destination
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int dest_length = _strlen(dest);
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[dest_length + i] = src[i];
+	}
+	dest[dest_length + i] = '\0';
+
+	return (dest);
+}
+
+/**
+ * _strlen - computes the length of a string
+ * @s: string input
+ *
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
