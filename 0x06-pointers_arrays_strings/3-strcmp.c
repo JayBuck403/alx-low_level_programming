108611679e431838e23baeb760cf4837fc8f7966
Theophilus Tay <jaybuck403@gmail.com>
2022-12-21 09:13:27 -0800

    Strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: string input
+ * @s2: string input
+ *
+ * Return: 0 if strings are equal, 15 if s1 is greater than s2 and -15 if s1 is less than s2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0; ; i++)
+	{
+		if (s1[i] == '\0' && s2[i] == '\0')
+		{
+			return (0);
+		}
+
+	       	j = (int) s1[i];
+		k = (int) s2[i];
+
+		if (j > k)
+			return (15);
+		if (k > j)
+			return (-15);
+	}
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -1,3 +1,4 @@
 0-strcat concatenates two strings
 1-strncat concatenates two strings
 2-strncpy copies a string
+3-strcmp compares two strings
