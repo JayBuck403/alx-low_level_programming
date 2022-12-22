dba831f04d323c114079a3e9efc4efba73bb8f5f
Theophilus Tay <jaybuck403@gmail.com>
2022-12-22 02:04:51 -0800

    Capitalize on what comes

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+
+char *cap_string(char *s)
+{
+	int i = 0;
+	int j;
+	char sep[] = {' ', '\t', '\n', ',', ';', '.', '!', '"', '(', ')', '{', '}'};
+
+	if (s[0] >= 'a' && s[0] <= 'z')
+	{
+		s[0] -= 32;
+	}
+	while (s[i] != '\0')
+	{
+		for (j = 0; sep[j] != '\0'; j++)
+		{
+			if (s[i - 1] == sep[j] && s[i] <= 122 && s[i] >= 97)
+				s[i] = s[i] - 32;
+		}
+		i++;
+	}
+	return (s);
+}
