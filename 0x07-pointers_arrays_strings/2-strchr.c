2a7775aad4d490104a538fd85adc8f949382ff2c
Theophilus Tay <jaybuck403@gmail.com>
2023-01-03 12:36:09 -0800

    Strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stddef.h>
+
 
 /**
  * _strchr - locates a character in a string
@@ -19,5 +19,5 @@ char *_strchr(char *s, char c)
 		}
 		s++;
 	}
-	return (NULL);
+	return (0);
 }
