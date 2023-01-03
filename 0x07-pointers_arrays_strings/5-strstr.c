9402e9b102c7b455656772d43c3d031116dd9154
Theophilus Tay <jaybuck403@gmail.com>
2023-01-03 13:55:39 -0800

    ChessBoard

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+
+
+/**
+ *_strstr - find the first occurence
+ *@haystack: string input
+ *@needle: string input
+ *
+ *Return: the pointer to the first occurence
+ */
+
+
+char *_strstr(char *haystack, char *needle)
+{
+
+}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -3,3 +3,5 @@
 2-strchr locates a character in a string
 3-strspn gets the length of a prefix substring
 4-strpbrk searches a string for any of a set of bytes
+5-strstr locates a substring
+7-print_chessboard prints a chessboard
