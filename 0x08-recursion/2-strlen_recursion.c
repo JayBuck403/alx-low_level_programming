b213b6e8b66b903b7acde5b67715b8145bd9da14
Theophilus Tay <jaybuck403@gmail.com>
2023-01-05 10:25:21 -0800

    Strlen

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string input
+ *
+ * Return: integer
+ */
+
+int _strlen_recursion(char *s)
+{
+	int i;
+
+	if (*s == '\0')
+	{
+		return (0);
+	}
+
+	i = _strlen_recursion(s + 1) + 1;
+	return (i);
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -1,3 +1,4 @@
 #Recursion
 0-puts_recursion prints a string
 1-print_re_recursion prints a string in reverse
+2-strlen_recursion prints the length of a string
