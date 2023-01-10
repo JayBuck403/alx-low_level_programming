d4c2765938e16ff620e289c7c66fc402af7ca99b
Theophilus Tay <jaybuck403@gmail.com>
2023-01-10 11:19:03 -0800

    Args

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ *main - multiplies two integers
+ *@argc: number of the arguments
+ *@argv: array of pointers to the strings
+ *
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i = 1;
+	int j = 2;
+	int c = 3;
+	if (argc != c)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[i]) * atoi(argv[j]));
+	return (0);
+}
