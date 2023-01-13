fe31ec44b5cb6d2babab9ffe762ed5024625d01a
Theophilus Tay <jaybuck403@gmail.com>
2023-01-13 03:22:10 -0800

    More Malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -26,24 +26,21 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		;
 	for (j = 0; s2[j]; j++)
 		;
-
 	if (j > n)
 		j = n;
 
-	c = malloc ((sizeof(char) * i) + (sizeof(char) * j) +1);
+	c = malloc ((sizeof(char) * i) + (sizeof(char) * j) + 1);
 
 	if (c == NULL)
 	{
 		return (NULL);
 	}
-
 	while (*s1 != '\0')
 	{
 		c[a] = *s1;
 		s1++;
 		a++;
 	}
-
 	while (a < j + i)
 	{
 		c[a] = *s2;
