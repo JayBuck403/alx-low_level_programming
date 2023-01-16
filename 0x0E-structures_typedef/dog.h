23ff04dbf340495afc4bcfb5ca65903d13440221
Theophilus Tay <jaybuck403@gmail.com>
2023-01-16 12:11:55 -0800

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,12 @@
+/**
+ * struct dog - new structure for dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+ *
+ * Description: blueprint for creating dog objects
+ */
+
 struct dog
 {
 	char *name;
