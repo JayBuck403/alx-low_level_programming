834d1209c6859d0ccf45906cd1cad02a13b82e3b
Theophilus Tay <jaybuck403@gmail.com>
2023-01-18 14:33:15 -0800

    Action

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a function given as a parameter
+ * @array: array input
+ * @size: size input
+ * @action: callback function
+ *
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array != NULL && action != NULL)
+	{
+		for (i = 0; i < size; i++)
+			action(array[i]);
+	}
+}
